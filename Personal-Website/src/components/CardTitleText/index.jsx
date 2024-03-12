415879b1eb64ebf114c102a2d3ac6d03d90525f6
Artur <artur.piasecki158@gmail.comgit config --global user.email artur.piasecki158@gmail.comgit config --list$ git config --global user.email artur.piasecki158@gmail.com>
2024-03-12 21:38:45 +0000

    NewVersion

--- Personal-Website/src/components/CardTitleText/index.jsx
+++
@@ -1,7 +0,0 @@
-import CardContext from "../../utils/CardContext";
-
-const CardTitleText = () => {
-  return <CardContext.Consumer>{ ({ title }) => <h2>{ title }</h2> }</CardContext.Consumer>;
-};
-
-export default CardTitleText;
