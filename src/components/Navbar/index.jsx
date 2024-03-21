f9ec701f7989d1f666d287e03ce423bd7f7a089c
brooksy236 <david.brooks1980@gmail.com>
2024-03-21 12:58:12 +0000

    fixed 'navbar collapse button not working' issue

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,7 +5,7 @@ import './style.css';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg sticky-top navbar-light bg-light">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
          Personal Website
